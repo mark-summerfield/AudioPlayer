673e554e8d6a37ae9e48aa10731100ad5fc6363b
Mark <mark@qtrac.eu>
2021-08-03 09:04:55 +0100

    Alternative version with no global variables...
    and slightly nicer code?

--- example/playerGUI.py
+++ example/playerGUI.py
@@ -1,90 +1,120 @@
-from audioplayer import AudioPlayer
-import tkinter
-from tkinter import filedialog, messagebox
+#!/usr/bin/env python3
+
 import os
-from platform import system
+import platform
+import tkinter
+import tkinter.filedialog
+import tkinter.messagebox
 
-paused = False
-player = None
-buttons_glyph = ('⏏','▶', '⏯' ,'⏹')  if system() == 'Windows' else ('⏏️', '▶️', '⏯️', '⏹️')
-
-def load():
-    global player, root
-    fname = filedialog.askopenfilename()
-    if fname:
-        player = AudioPlayer(fname)
-        changevolume(0)  # update UI
-        namelabel.config(text=os.path.basename(player.fullfilename))
-        try:
-            player.play()
-        except Exception as e:
-            messagebox.showerror('Error', e)
+import audioplayer
 
 
-def tooglepause():
-    global player, paused
-    if not player is None:
-        if paused:
-            player.resume()
-        else:
-            player.pause()
-        paused = not paused
+def main():
+    player = Player()
+    root = tkinter.Tk()
+    config_root(root)
+    make_widgets(root, player)
+    root.mainloop()
 
 
-def play():
-    global player
-    if not player is None:
-        try:
-            player.play()
-        except Exception as e:
-            messagebox.showerror('Error', e)
+def config_root(root):
+    root.title('Music Player')
+    root.attributes('-topmost', True)
+    root.resizable(False, False)
+    root.attributes('-topmost', False)
 
 
-def stop():
-    global player
-    if not player is None:
-        player.stop()
+def make_widgets(root, player):
+    botframe = tkinter.Frame()
+    botframe.pack(fill=tkinter.X, side=tkinter.TOP)
+    namelabel = tkinter.Label(botframe, anchor=tkinter.W, font=LABELFONT)
+    namelabel.pack(fill=tkinter.X, expand=1, side=tkinter.LEFT, padx=2)
+    vollabel = tkinter.Label(botframe, text='100%', anchor=tkinter.E,
+                             font=LABELFONT)
+    vollabel.pack(side=tkinter.LEFT, padx=0)
+
+    player.namelabel = namelabel
+    player.vollabel = vollabel
+
+    toolbar = tkinter.Frame(root)
+    toolbar.pack(side=tkinter.TOP)
+    tkinter.Button(toolbar, text=BUTTONS_GLYPH[0], font=BUTTONFONT,
+                   width=2, command=player.load).pack(side=tkinter.LEFT)
+    tkinter.Button(toolbar, text=BUTTONS_GLYPH[1], font=BUTTONFONT,
+                   width=2, command=player.play).pack(side=tkinter.LEFT)
+    tkinter.Button(toolbar, text=BUTTONS_GLYPH[2], font=BUTTONFONT,
+                   width=2,
+                   command=player.tooglepause).pack(side=tkinter.LEFT)
+    tkinter.Button(toolbar, text=BUTTONS_GLYPH[3], font=BUTTONFONT,
+                   width=2, command=player.stop).pack(side=tkinter.LEFT)
+
+    volframe = tkinter.Frame(toolbar)
+    volframe.pack(side=tkinter.LEFT, expand=1, fill=tkinter.BOTH)
+    tkinter.Button(
+        volframe, text='➕',
+        command=lambda player=player: player.changevolume(10)).pack(
+        side=tkinter.TOP, expand=1, fill=tkinter.BOTH)
+    tkinter.Button(
+        volframe, text='➖',
+        command=lambda player=player: player.changevolume(-10)).pack(
+        side=tkinter.TOP, expand=1, fill=tkinter.BOTH)
 
 
-def changevolume(delta):
-    global player, vollabel
-    if not player is None:
-        player.volume += delta
-        vollabel.config(text='{}%'.format(player.volume))
+BUTTONFONT = (None, 30)
+LABELFONT = (None, 8)
+BUTTONS_GLYPH = (('⏏', '▶', '⏯', '⏹') if platform.system() == 'Windows' else
+                 ('⏏️', '▶️', '⏯️', '⏹️'))
 
 
-btnfont = (None, 30)
-lblfont = (None, 8)
+class Player:
 
-# Build UI
-root = tkinter.Tk()
-root.title('Music Player')
-root.attributes('-topmost', True)
-root.resizable(False, False)
-root.attributes('-topmost', False)
+    def __init__(self):
+        self.vollabel = None
+        self.namelabel = None
+        self.paused = False
+        self.player = None
 
-botframe = tkinter.Frame()
-botframe.pack(fill=tkinter.X, side=tkinter.TOP)
-namelabel = tkinter.Label(botframe,
-                          anchor=tkinter.W, font=lblfont)
-namelabel.pack(fill=tkinter.X, expand=1, side=tkinter.LEFT, padx=2)
-vollabel = tkinter.Label(botframe, text='100%', anchor=tkinter.E, font=lblfont)
-vollabel.pack(side=tkinter.LEFT, padx=0)
 
-toolbar = tkinter.Frame(root)
-toolbar.pack(side=tkinter.TOP)
-tkinter.Button(toolbar, text=buttons_glyph[0], font=btnfont, width=2,
-               command=load).pack(side=tkinter.LEFT)
-tkinter.Button(toolbar, text=buttons_glyph[1], font=btnfont, width=2,
-               command=play).pack(side=tkinter.LEFT)
-tkinter.Button(toolbar, text=buttons_glyph[2], font=btnfont, width=2,
-               command=tooglepause).pack(side=tkinter.LEFT)
-tkinter.Button(toolbar, text=buttons_glyph[3], font=btnfont, width=2,
-               command=stop).pack(side=tkinter.LEFT)
+    def load(self):
+        fname = tkinter.filedialog.askopenfilename()
+        if fname:
+            self.player = audioplayer.AudioPlayer(fname)
+            self.changevolume(0)  # update UI
+            self.namelabel.config(
+                text=os.path.basename(self.player.fullfilename))
+            try:
+                self.player.play()
+            except Exception as err:
+                tkinter.messagebox.showerror('Error', err)
 
-volframe = tkinter.Frame(toolbar)
-volframe.pack(side=tkinter.LEFT, expand=1, fill=tkinter.BOTH)
-tkinter.Button(volframe, text='➕', command=lambda: changevolume(10)).pack(side=tkinter.TOP, expand=1, fill=tkinter.BOTH)
-tkinter.Button(volframe, text='➖', command=lambda: changevolume(-10)).pack(side=tkinter.TOP, expand=1, fill=tkinter.BOTH)
 
-root.mainloop()
+    def tooglepause(self):
+        if self.player is not None:
+            if self.paused:
+                self.player.resume()
+            else:
+                self.player.pause()
+            self.paused = not self.paused
+
+
+    def play(self):
+        if self.player is not None:
+            try:
+                self.player.play()
+            except Exception as err:
+                tkinter.messagebox.showerror('Error', err)
+
+
+    def stop(self):
+        if self.player is not None:
+            self.player.stop()
+
+
+    def changevolume(self, delta):
+        if self.player is not None:
+            self.player.volume += delta
+            self.vollabel.config(text='{}%'.format(self.player.volume))
+
+
+if __name__ == '__main__':
+    main()
